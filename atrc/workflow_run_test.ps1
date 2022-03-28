cc388050c9107f0a88c6f13c8afa52cac0c33ba6
Pronab <Pronab.pal@unimelb.edu.au>
2022-03-28 15:46:45 +1100

    rename data dir used to atrc_data

--- atrc/workflow_run_test.ps1
+++ atrc/workflow_run_test.ps1
@@ -6,14 +6,14 @@
 docker build -t matsimwrapper-atrc:latest -f ./atrc/Dockerfile .
 
 # Create folders for data input and output within ATRC sub dir:
-mkdir -Force ./atrc/data
-mkdir -Force ./atrc/data/inputs
-mkdir -Force ./atrc/data/outputs
+mkdir -Force ./atrc/atrc_data
+mkdir -Force ./atrc/atrc_data/inputs
+mkdir -Force ./atrc/atrc_data/outputs
 
 # Copy over test parameters and files:
-cp ./src/test/resources/Melbourne/* ./atrc/data/inputs/
-cp ./atrc/test_parameters.yaml ./atrc/data/parameters.yaml
+cp ./src/test/resources/Melbourne/* ./atrc/atrc_data/inputs/
+cp ./atrc/test_parameters.yaml ./atrc/atrc_data/parameters.yaml
 
 # Run the container:
-docker run --mount type=bind,source=${PWD}/atrc/data,target=/data matsimwrapper-atrc:latest
+docker run --mount type=bind,source=${PWD}/atrc/atrc_data,target=/atrc_data matsimwrapper-atrc:latest
     
--- atrc/workflow_run_test.sh
+++ atrc/workflow_run_test.sh
@@ -8,16 +8,16 @@
 docker build -t matsimwrapper-atrc -f atrc/Dockerfile .
 
 # Create folders for data input and output:
-mkdir data
-mkdir data/inputs
-mkdir data/outputs
+mkdir atrc_data
+mkdir atrc_data/inputs
+mkdir atrc_data/outputs
 
 # Copy over test parameters and files:
-cp src/test/resources/Melbourne/* data/inputs/
-cp atrc/test_parameters.yaml data/parameters.yaml
+cp src/test/resources/Melbourne/* atrc_data/inputs/
+cp atrc/test_parameters.yaml atrc_data/parameters.yaml
 
 # Run the container:
 docker run \
-    --mount type=bind,source="$(pwd)"/data,target=/data \
+    --mount type=bind,source="$(pwd)"/atrc_data,target=/atrc_data \
     matsimwrapper-atrc
     
